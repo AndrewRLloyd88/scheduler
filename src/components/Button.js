70e31aec3e0f01a5c3526fbf1164039df863e98c
Andrew Lloyd <andrew.lloyd@googlemail.com>
2020-09-28 20:12:50 -0700

    refactor button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,15 @@
 import React from "react";
+import classNames from 'classnames';
 import "components/Button.scss";
+
+
 const Button = (props) => {
-  let buttonClass = "button";
-  if (props.confirm) {
-    buttonClass += " button--confirm";
-  }
-  if (props.danger) {
-    buttonClass += " button--danger";
-  }
+  const buttonClass = classNames("button", {
+
+    "button--confirm" : props.confirm,
+    "button--danger" : props.danger
+  });
+  
   return (
     <button
       className={buttonClass}
