930a06cfe0d458ce7747f4e0c1aff44f6e58c03f
Andrew Lloyd <andrew.lloyd@googlemail.com>
2020-09-29 18:24:28 -0700

    prevent default to stop app crashing on form submit

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,13 +5,25 @@ import Button from "../Button";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  //Clears the reset form
+  const reset = (e) => {
+    setName("");
+    setInterviewer(null);
+  };
+  //Cancel Action on our Form
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             value={name}
-            onChange={(event) => setName(event.target.value)}
+            onChange={(e) => setName(e.target.value)}
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
@@ -29,10 +41,10 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>
+          <Button danger onClick={() => cancel()}>
             Cancel
           </Button>
-          <Button confirm onClick={props.onSave}>
+          <Button confirm onClick={() => reset()}>
             Save
           </Button>
         </section>
