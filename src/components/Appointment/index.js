aa319bef961c3c5dac7fc21f83a809d6fcaa4fc4
Andrew Lloyd <andrew.lloyd@googlemail.com>
2020-10-01 10:34:33 -0700

    add the mode constants to the src/components/Appointment/index.js file. As we continue through the activities, we will need to add more constants to this list. Start out with EMPTY and SHOW

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,10 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 //building our appointment component
 export default function Appointment(props) {
