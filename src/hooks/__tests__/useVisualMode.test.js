51e430a4e424dc396cabe665189d0c58286dd25d
Andrew Lloyd <andrew.lloyd@googlemail.com>
2020-10-01 08:18:47 -0700

    add test for transition state, is currently failing in Jest need to write transition function within useVisualMode

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -1,11 +1,22 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 
 import useVisualMode from "hooks/useVisualMode";
-
+//setting the initial mode
 const FIRST = "FIRST";
+//Transition
+const SECOND = "SECOND";
 
+//test for initial mode
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   expect(result.current.mode).toBe(FIRST);
+});
+
+//This test will initialize the mode to FIRST, then transition to SECOND and then check to see what the current value of mode is.
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
 });
