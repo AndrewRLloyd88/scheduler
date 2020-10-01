ed1ac6e5527e8e4674ec9dccf1e9baa99367f5b9
Andrew Lloyd <andrew.lloyd@googlemail.com>
2020-10-01 09:08:51 -0700

    PASS  src/hooks/__tests__/useVisualMode.test.js add transition state and pass test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,9 +3,15 @@ import { useState } from "react";
 //take in an initial mode (initialmode)
 export default function useVisualMode(initialmode) {
   //set the mode state with the initial mode provided
-
   const [mode, setMode] = useState(initialmode);
 
+  //adding the transitition function that will take in a new mode
+  const transition = (newmode) => {
+    //update the mode state to the newmode value and return
+    return setMode(newmode);
+  };
+
   // return an object with a mode property
-  return { mode };
+  //add the transition property to the object that useVisualMode returns
+  return { mode, transition };
 }
