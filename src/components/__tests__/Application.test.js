238bbe00b55f859aea25be2407c26fe2c049c146
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-06 23:34:14 -0700

    finish The Next Tests exercise W8 D2

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -49,7 +49,7 @@ describe("Application", () => {
     const day = getAllByTestId(container, "day").find((day) =>
       queryByText(day, "Monday")
     );
-    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+    expect(getByText(day, "3 spots remaining")).toBeInTheDocument();
   });
 
   it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
@@ -80,7 +80,7 @@ describe("Application", () => {
     const day = getAllByTestId(container, "day").find((day) =>
       queryByText(day, "Monday")
     );
-    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+    expect(getByText(day, "5 spots remaining")).toBeInTheDocument();
   });
 
   it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
@@ -117,7 +117,7 @@ describe("Application", () => {
     const day = getAllByTestId(container, "day").find((day) =>
       queryByText(day, "Monday")
     );
-    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+    expect(getByText(day, "4 spots remaining")).toBeInTheDocument();
   });
 
   it("shows the save error when failing to save an appointment", async () => {
