f998b7af8d63fdf9d488c962a9b9a8404ff7707d
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-07 16:55:37 -0700

    change web config

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -15,7 +15,7 @@ import {
 
 import Application from "components/Application";
 import axios from "axios";
-
+process.env.REACT_APP_WEBSOCKET_URL = "ws://localhost:8001";
 afterEach(cleanup);
 
 describe("Application", () => {
